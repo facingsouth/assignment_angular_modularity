6ca3416755bbf1bf7bbf957d798d72fa8025dc49
Garrett <garrettweinert@gmail.com>
2015-09-15 17:40:40 -0700

    uhfu

--- controllers/puppiesController.js
+++ controllers/puppiesController.js
@@ -5,16 +5,41 @@ puppyApp.controller('puppyCtrl', [
   function($scope, breedsService, puppiesService) {
     $scope.puppies = [];
     $scope.breeds = [];
+    $scope.tableDisplay = true;
 
     breedsService.getAllBreeds().then(
       function(response) { $scope.breeds = response.data; console.log(response); },
       function(data, status, headers, config) { console.log(status) }
     );
 
+    $scope.createNewBreed = function(){
+
+      breedsService.postNewBreed($scope.puppyBreed)
+      .then(function(response){
+        
+        console.log(response.data)
+        $scope.breeds.push(response.data);
+        console.log("created new breed");
+      
+      }, function(){
+        console.log("failed to post new breed");
+      
+      });
+    };
+
+    $scope.toggleTable = function(){
+      $scope.tableDisplay = !$scope.tableDisplay;
+    };
+
     puppiesService.getAllPuppies().then(
       function(response) { $scope.puppies = response.data; console.log(response); },
       function(data, status, headers, config) { console.log(status) }
     );
+
+    $scope.completePuppyBreed = function(breed){
+      $scope.puppyBreed = breed.name;
+      $scope.tableDisplay = true;
+    }
 
     $scope.submitPuppy = function(){
       var newPuppy = {
